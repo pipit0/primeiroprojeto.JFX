c7613419e0e5b0cb8887d992bf372a8fc0a40ea0
pipit0 <rian.amaral1@outlook.com>
2023-06-12 20:31:22 -0300

    Versionamento inicial do projeto para a turma de Ti 2022 Noturno do Senac (primeiro projeto)

---
+++ src/main/java/sp/senac/br/olamundo/TesteController.java
@@ -0,0 +1,36 @@
+package sp.senac.br.olamundo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TesteController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private TextField textomensagem;
+
+    public void abrirTelaHello(ActionEvent actionEvent) throws IOException {
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();*/
+
+
+
+
+
+        welcomeText.setText(textomensagem.getText());
+
+    }
+}
